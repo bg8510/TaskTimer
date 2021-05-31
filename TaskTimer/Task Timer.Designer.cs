a64cd269cad7f4c461bc550b1c895fc725aebda4
Brooks Gatlin <gbrooks8510@excite.com>
2021-05-31 17:56:41 -0500

    Moved the controls tighter together vertically

--- TaskTimer/Task Timer.Designer.cs
+++ TaskTimer/Task Timer.Designer.cs
@@ -132,7 +132,7 @@
             this.btnReset2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnReset2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnReset2.ForeColor = System.Drawing.Color.White;
-            this.btnReset2.Location = new System.Drawing.Point(602, 128);
+            this.btnReset2.Location = new System.Drawing.Point(602, 104);
             this.btnReset2.Name = "btnReset2";
             this.btnReset2.Size = new System.Drawing.Size(73, 35);
             this.btnReset2.TabIndex = 9;
@@ -149,7 +149,7 @@
             this.btnStopContinue2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnStopContinue2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnStopContinue2.ForeColor = System.Drawing.Color.White;
-            this.btnStopContinue2.Location = new System.Drawing.Point(507, 128);
+            this.btnStopContinue2.Location = new System.Drawing.Point(507, 104);
             this.btnStopContinue2.Name = "btnStopContinue2";
             this.btnStopContinue2.Size = new System.Drawing.Size(89, 35);
             this.btnStopContinue2.TabIndex = 8;
@@ -164,7 +164,7 @@
             this.btnStart2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnStart2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnStart2.ForeColor = System.Drawing.Color.White;
-            this.btnStart2.Location = new System.Drawing.Point(437, 128);
+            this.btnStart2.Location = new System.Drawing.Point(437, 104);
             this.btnStart2.Name = "btnStart2";
             this.btnStart2.Size = new System.Drawing.Size(63, 35);
             this.btnStart2.TabIndex = 7;
@@ -179,7 +179,7 @@
             this.lblTime2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.lblTime2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTime2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(130)))), ((int)(((byte)(101)))));
-            this.lblTime2.Location = new System.Drawing.Point(375, 136);
+            this.lblTime2.Location = new System.Drawing.Point(375, 112);
             this.lblTime2.Name = "lblTime2";
             this.lblTime2.Size = new System.Drawing.Size(34, 19);
             this.lblTime2.TabIndex = 6;
@@ -191,7 +191,7 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.timer2Description.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.timer2Description.Location = new System.Drawing.Point(26, 132);
+            this.timer2Description.Location = new System.Drawing.Point(26, 108);
             this.timer2Description.Name = "timer2Description";
             this.timer2Description.Size = new System.Drawing.Size(329, 27);
             this.timer2Description.TabIndex = 5;
@@ -204,7 +204,7 @@
             this.btnReset3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnReset3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnReset3.ForeColor = System.Drawing.Color.White;
-            this.btnReset3.Location = new System.Drawing.Point(602, 198);
+            this.btnReset3.Location = new System.Drawing.Point(602, 150);
             this.btnReset3.Name = "btnReset3";
             this.btnReset3.Size = new System.Drawing.Size(73, 35);
             this.btnReset3.TabIndex = 14;
@@ -221,7 +221,7 @@
             this.btnStopContinue3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnStopContinue3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnStopContinue3.ForeColor = System.Drawing.Color.White;
-            this.btnStopContinue3.Location = new System.Drawing.Point(507, 198);
+            this.btnStopContinue3.Location = new System.Drawing.Point(507, 150);
             this.btnStopContinue3.Name = "btnStopContinue3";
             this.btnStopContinue3.Size = new System.Drawing.Size(89, 35);
             this.btnStopContinue3.TabIndex = 13;
@@ -236,7 +236,7 @@
             this.btnStart3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnStart3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnStart3.ForeColor = System.Drawing.Color.White;
-            this.btnStart3.Location = new System.Drawing.Point(437, 198);
+            this.btnStart3.Location = new System.Drawing.Point(437, 150);
             this.btnStart3.Name = "btnStart3";
             this.btnStart3.Size = new System.Drawing.Size(63, 35);
             this.btnStart3.TabIndex = 12;
@@ -251,7 +251,7 @@
             this.lblTime3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.lblTime3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTime3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(130)))), ((int)(((byte)(101)))));
-            this.lblTime3.Location = new System.Drawing.Point(375, 206);
+            this.lblTime3.Location = new System.Drawing.Point(375, 158);
             this.lblTime3.Name = "lblTime3";
             this.lblTime3.Size = new System.Drawing.Size(34, 19);
             this.lblTime3.TabIndex = 11;
@@ -263,7 +263,7 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.timer3Description.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.timer3Description.Location = new System.Drawing.Point(26, 202);
+            this.timer3Description.Location = new System.Drawing.Point(26, 154);
             this.timer3Description.Name = "timer3Description";
             this.timer3Description.Size = new System.Drawing.Size(329, 27);
             this.timer3Description.TabIndex = 10;
@@ -276,7 +276,7 @@
             this.btnReset4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnReset4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnReset4.ForeColor = System.Drawing.Color.White;
-            this.btnReset4.Location = new System.Drawing.Point(602, 269);
+            this.btnReset4.Location = new System.Drawing.Point(602, 195);
             this.btnReset4.Name = "btnReset4";
             this.btnReset4.Size = new System.Drawing.Size(73, 35);
             this.btnReset4.TabIndex = 19;
@@ -293,7 +293,7 @@
             this.btnStopContinue4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnStopContinue4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnStopContinue4.ForeColor = System.Drawing.Color.White;
-            this.btnStopContinue4.Location = new System.Drawing.Point(507, 269);
+            this.btnStopContinue4.Location = new System.Drawing.Point(507, 195);
             this.btnStopContinue4.Name = "btnStopContinue4";
             this.btnStopContinue4.Size = new System.Drawing.Size(89, 35);
             this.btnStopContinue4.TabIndex = 18;
@@ -308,7 +308,7 @@
             this.btnStart4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnStart4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnStart4.ForeColor = System.Drawing.Color.White;
-            this.btnStart4.Location = new System.Drawing.Point(437, 269);
+            this.btnStart4.Location = new System.Drawing.Point(437, 195);
             this.btnStart4.Name = "btnStart4";
             this.btnStart4.Size = new System.Drawing.Size(63, 35);
             this.btnStart4.TabIndex = 17;
@@ -323,7 +323,7 @@
             this.lblTime4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.lblTime4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTime4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(130)))), ((int)(((byte)(101)))));
-            this.lblTime4.Location = new System.Drawing.Point(375, 277);
+            this.lblTime4.Location = new System.Drawing.Point(375, 203);
             this.lblTime4.Name = "lblTime4";
             this.lblTime4.Size = new System.Drawing.Size(34, 19);
             this.lblTime4.TabIndex = 16;
@@ -335,7 +335,7 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.timer4Description.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.timer4Description.Location = new System.Drawing.Point(26, 272);
+            this.timer4Description.Location = new System.Drawing.Point(26, 198);
             this.timer4Description.Name = "timer4Description";
             this.timer4Description.Size = new System.Drawing.Size(329, 27);
             this.timer4Description.TabIndex = 15;
@@ -358,7 +358,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(711, 351);
+            this.ClientSize = new System.Drawing.Size(711, 243);
             this.Controls.Add(this.lblDescription);
             this.Controls.Add(this.btnReset4);
             this.Controls.Add(this.btnStopContinue4);
